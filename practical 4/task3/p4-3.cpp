ca4f6515ca20de6c2f9da1f76945d4c339e3ac63
Srishhtyy <91174546+Srishhtyy@users.noreply.github.com>
2021-10-02 21:20:19 +0530

    Add files via upload

---
+++ practical 4/task3/p4-3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+namespace first {
+	int add(int a, int b) {
+		return (a + b);
+	}
+}
+
+namespace second {
+	float add(float a, float b) {
+		return (a + b);
+	}
+}
+
+
+int main() {
+	int a, b;
+	float c, d;
+	cin >> a >> b >> c >> d;
+	cout << add(a, b) << endl;
+	cout << add(c, d);
+}
